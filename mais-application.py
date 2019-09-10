509ea2478b643a1fd26a0baf21560e06e35f6d0a
kushasareen <54873161+kushasareen@users.noreply.github.com>
2019-09-10 00:05:24 -0400

    Update mais-application.py

--- mais-application.py
+++ mais-application.py
@@ -5,18 +5,21 @@ import numpy as np
 
 loan_dict = defaultdict(dict)
 
-with open(r'C:\Users\kusha\Desktop\loan_data.csv', mode='r') as csv_file:
+# read relevant entries from files into a dictionary
+with open(r'C:\Users\kusha\Desktop\loan_data.csv', mode='r') as csv_file: # enter file path here
     csv_reader = csv.DictReader(csv_file)
 
     for row in csv_reader:
         loan_dict[f'{row["member_id"]}'][0] = f'{row["loan_amnt"]}'
 
-with open(r'C:\Users\kusha\Desktop\home_ownership_data.csv', mode='r') as csv_file:
+with open(r'C:\Users\kusha\Desktop\home_ownership_data.csv', mode='r') as csv_file: # enter file path here
     csv_reader = csv.DictReader(csv_file)
 
     for row in csv_reader:
         loan_dict[f'{row["member_id"]}'][1] = f'{row["home_ownership"]}'
 
+
+# interate through dictionary and compute average
 num_own = own_sum = num_mortgage = mortgage_sum = num_rent = rent_sum = 0
 for key in loan_dict:
     if loan_dict[key][1] == 'OWN':
@@ -33,12 +36,15 @@ mortgage_avg = mortgage_sum/num_mortgage
 own_avg = own_sum/num_own
 rent_avg = rent_sum/num_rent
 
+# store averages in a list
 avg = []
 avg.append(mortgage_sum/num_mortgage)
 avg.append(own_sum/num_own)
 avg.append(rent_sum/num_rent)
 
-with open(r'C:\Users\kusha\Desktop\average_loans.csv', mode='w', newline='') as csv_file:
+
+# write averages to a csv file
+with open(r'C:\Users\kusha\Desktop\average_loans.csv', mode='w', newline='') as csv_file: # enter file path here
     fieldnames = ['home_ownership', 'loan_amnt']
     csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
 
@@ -50,6 +56,7 @@ with open(r'C:\Users\kusha\Desktop\average_loans.csv', mode='w', newline='') as 
 label = ['MORTGAGE', 'OWN', 'RENT']
 
 
+# plots a bar graph with respective averages and home ownerships
 def plot_bar():
     index = np.arange(len(label))
     plt.bar(index, avg)
