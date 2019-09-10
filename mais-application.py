0151273c6c49cdc65427324ec93780b2b2d67c8b
kushasareen <54873161+kushasareen@users.noreply.github.com>
2019-09-09 23:52:56 -0400

    Add files via upload

---
+++ mais-application.py
@@ -0,0 +1,63 @@
+import csv
+from collections import defaultdict
+import matplotlib.pyplot as plt
+import numpy as np
+
+loan_dict = defaultdict(dict)
+
+with open(r'C:\Users\kusha\Desktop\loan_data.csv', mode='r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+
+    for row in csv_reader:
+        loan_dict[f'{row["member_id"]}'][0] = f'{row["loan_amnt"]}'
+
+with open(r'C:\Users\kusha\Desktop\home_ownership_data.csv', mode='r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+
+    for row in csv_reader:
+        loan_dict[f'{row["member_id"]}'][1] = f'{row["home_ownership"]}'
+
+num_own = own_sum = num_mortgage = mortgage_sum = num_rent = rent_sum = 0
+for key in loan_dict:
+    if loan_dict[key][1] == 'OWN':
+        num_own += 1
+        own_sum += int(loan_dict[key][0])
+    elif loan_dict[key][1] == 'MORTGAGE':
+        num_mortgage += 1
+        mortgage_sum += int(loan_dict[key][0])
+    elif loan_dict[key][1] == 'RENT':
+        num_rent += 1
+        rent_sum += int(loan_dict[key][0])
+
+mortgage_avg = mortgage_sum/num_mortgage
+own_avg = own_sum/num_own
+rent_avg = rent_sum/num_rent
+
+avg = []
+avg.append(mortgage_sum/num_mortgage)
+avg.append(own_sum/num_own)
+avg.append(rent_sum/num_rent)
+
+with open(r'C:\Users\kusha\Desktop\average_loans.csv', mode='w') as csv_file:
+    fieldnames = ['home_ownership', 'loan_amnt']
+    csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
+    csv_writer.writeheader()
+    csv_writer.writerow({'home_ownership': 'MORTGAGE', 'loan_amnt': avg[0]})
+    csv_writer.writerow({'home_ownership': 'OWN', 'loan_amnt': avg[1]})
+    csv_writer.writerow({'home_ownership': 'RENT', 'loan_amnt': avg[2]})
+
+label = ['MORTGAGE', 'OWN', 'RENT']
+
+
+def plot_bar():
+    index = np.arange(len(label))
+    plt.bar(index, avg)
+    plt.xlabel('Home Ownership')
+    plt.ylabel('Average Loan Amount ($)')
+    plt.xticks(index, label)
+    plt.title('Average loan amounts per home ownership')
+    plt.show()
+
+
+plot_bar()
